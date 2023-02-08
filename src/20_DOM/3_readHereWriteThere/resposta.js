eb8863336b31d7bcfcde573dd0ac43ef0d750207
Vero-SB <verosb.dev@gmail.com>
2023-02-08 21:00:11 +0100

    dom ex3

--- src/20_DOM/3_readHereWriteThere/resposta.js
+++ src/20_DOM/3_readHereWriteThere/resposta.js
@@ -4,6 +4,17 @@
  */
 
 // Escribe aquí o teu código.
+const entradaDoTexto = document.querySelector("textarea")
+const saidaDoResultado = document.querySelector("#enMaiusculas")
+
+function actualizaSaidaEnMaiusculas() {
+    const textoIntroducido = entradaDoTexto.value
+    const textoEnMaiusculas = textoIntroducido.toUpperCase()
+    saidaDoResultado.innerText = textoEnMaiusculas
+}
+
+entradaDoTexto.addEventListener(actualizaSaidaEnMaiusculas)
+
 
 /**
  * Non cambies nada despois destas liñas
